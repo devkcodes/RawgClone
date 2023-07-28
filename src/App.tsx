0f22d834d722c61524dfc1fa93fe243c554a1de7
devkcodes <devanshkatheria8111@gmail.com>
2023-07-29 01:11:55 +0530

    made navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
-import './App.css'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return( 
- <>
-  <Button colorScheme='blue'> HI!</Button>
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav">
+          <NavBar />
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="red">
+            aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="blue">
+          main
+        </GridItem>
+      </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
