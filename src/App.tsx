7a4c9a387f001d2440e40bf88a430fd35c038571
devkcodes <devanshkatheria8111@gmail.com>
2023-07-29 03:25:21 +0530

    api integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
 
 function App() {
   return (
@@ -15,12 +16,10 @@ function App() {
           <NavBar />
         </GridItem>
         <Show above="lg">
-          <GridItem area="aside" bg="red">
-            aside
-          </GridItem>
+          <GridItem area="aside">aside</GridItem>
         </Show>
-        <GridItem area="main" bg="blue">
-          main
+        <GridItem area="main">
+          <GameGrid />
         </GridItem>
       </Grid>
     </>
---
+++ src/services/api-client.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://api.rawg.io/api",
+  params: {
+    key: "24a90e8750d344159e7d29887f8ec3e6",
+  },
+});
